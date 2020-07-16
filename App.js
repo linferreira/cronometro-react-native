dbcde68acd728dbaed1092b1b73ac1baa796eb4b
Lindsay Ferreira <eson@iouu.com.br>
2020-07-16 15:26:36 -0300

    criar cronometro

--- App.js
+++ App.js
@@ -1,12 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [timer, setTimer] = useState(0)
+  const [active, setActive] = useState(null)
+
+  const start = () => {
+    let time = timer
+
+    if (active !== null) {
+      clearInterval(active);
+      setActive(null);
+    } else {
+      setActive(setInterval(() => {
+        time += 0.1
+      }, 100))
+      setTimer(time)
+    }
+  }
+
+  const limpar = () => {
+    if (active !== null) {
+      clearInterval(active);
+      setActive(null);
+    }
+    setTimer(0);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style={styles.cronometro} source={require('./src/cronometro.png')} />
+
+      <Text style={styles.timer}>{timer.toFixed(1)}</Text>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={() => start()}>
+          <Text style={styles.buttontext}>
+            Começar
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => limpar()} >
+          <Text style={styles.buttontext}>
+            Limpar
+          </Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
   );
 }
@@ -14,8 +54,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
+  timer: {
+    color: '#fff',
+    fontSize: 65,
+    fontWeight: 'bold',
+    marginTop: -160
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 100,
+    height: 40,
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    height: 40,
+    margin: 20,
+    borderRadius: 5,
+  },
+  buttontext: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  }
+
 });
